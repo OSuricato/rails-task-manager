bf19373b2c57f9ec8260314cbb137f8bc7a64c7e
victor barbosa <barbosavictor30@gmail.com>
2023-08-22 16:00:08 -0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # ? see all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # ? create new task
+  get 'task/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # ? see task detail
+  get 'tasks/:id', to: 'tasks#details', as: :task
+
+  # ? edit task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # ? remove task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
